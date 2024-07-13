1384481ae8548b119caa9dbdcbb7cb3f7950d3fc
DARIUS MARK ROY SIAT CAPARRO <152949767+darthecodemaster@users.noreply.github.com>
2024-07-13 08:46:55 +0800

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed= new Typed('.text', {
+    strings: ["Front-End Developer", "Programmer", "Web Designer"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+});
